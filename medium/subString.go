fc7317a275e5f2b560926973619f602f8aa2c592
huangzhipeng <huangzhipeng@wrdtech.com>
2020-07-08 16:24:25 +0800

    [feature] solve the question of lengthOfLongestSubstring

--- medium/subString.go
+++ medium/subString.go
@@ -1,11 +1,26 @@
 package medium
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 //给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
 func lengthOfLongestSubstring(s string) int {
+	start, end := 0, 0
+	for i := 0; i < len(s); i++ {
+		index := strings.Index(s[start:i], string(s[i]))
+		if index == -1 {
+			if i+1 > end {
+				end = i + 1
+			}
+		} else {
+			start += index + 1
+			end += index + 1
+		}
+	}
+	return end - start
 
-	return 1
 }
 
 func TestLenOfLongestS() {
